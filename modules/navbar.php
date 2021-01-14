99c9d213210dc65060930d4f218847eaf507720b
soldochris <soldevilla.christian@gmail.com>
2021-01-14 01:08:53 -0500

    navbar updated

--- modules/navbar.php
+++ modules/navbar.php
@@ -7,7 +7,7 @@
       <a class="navbar-brand" href="index.php">Christian Soldevilla</a>
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+          <a class="nav-link" aria-current="page" href="index.php">Home</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="assignments.php">Assignements</a>
